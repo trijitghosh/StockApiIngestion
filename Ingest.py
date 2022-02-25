c1624a31536405f327052fb7462e3ce35ed708f8
Trijit Ghosh <trijitghosh@gmail.com>
2022-02-25 10:27:30 +0000

    changes for sorting the data

--- Ingest.py
+++ Ingest.py
@@ -1,4 +1,5 @@
 import csv
+import time
 from json import dumps
 
 import requests
@@ -10,17 +11,20 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x:
                          dumps(x).encode('utf-8'))
 
+
+def return_epoch(elem):
+    return int(time.mktime(time.strptime(elem[0], '%Y-%m-%d %H:%M:%S')))
+
+
 with requests.Session() as s:
     download = s.get(CSV_URL)
     decoded_content = download.content.decode('utf-8')
     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    data = "'time': {time}, 'open': {open}, 'high': {high}, 'low':{low}, 'close': {close}, 'symbol':'IBM'"
-    next(cr)
-    while True:
-        try:
-            row = next(cr)
-            producer.send('time_series_intraday_prices', '{'+data.format(time=row[0], open=float(row[1]), high=float(row[2]), low=float(row[3]), close=float(row[4]))+'}')
-        except StopIteration as e:
-            break
+    my_list = list(cr)
+    my_list.pop(0)
+    my_list = sorted(my_list, key=return_epoch)
+    for row in my_list:
+        producer.send('time_series_intraday_prices', dict(time=row[0], open=float(row[1]), high=float(row[2]),
+                                                          low=float(row[3]), close=float(row[4]), symbol='IBM'))
 
     producer.flush()
